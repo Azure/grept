2d371753e1f21ba137971102f818f07f078afb89
zjhe <hezijie@microsoft.com>
2023-11-10 14:42:11 +0800

    fix temp dir path for remote config to fix incorrect go-get git default branch

--- cmd/config_path.go
+++ cmd/config_path.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/Azure/grept/pkg"
+	"github.com/google/uuid"
 	"github.com/hashicorp/go-getter/v2"
 	"github.com/spf13/afero"
 	"os"
+	"path/filepath"
 )
 
 func getConfigFolder(path string, ctx context.Context) (configPath string, onDefer func(), err error) {
@@ -15,10 +17,7 @@ func getConfigFolder(path string, ctx context.Context) (configPath string, onDef
 	if exists && err == nil {
 		return path, nil, nil
 	}
-	tmp, err := os.MkdirTemp("", "grept")
-	if err != nil {
-		return "", nil, err
-	}
+	tmp := filepath.Join(os.TempDir(), uuid.NewString())
 	cleaner := func() {
 		_ = os.RemoveAll(tmp)
 	}
