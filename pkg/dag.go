976e8b7b99a9864a96cfc6704170152d58decba0
hezijie <lonegunmanb@hotmail.com>
2023-12-12 10:19:42 +0800

    wrap dag into a new type

--- pkg/dag.go
+++ pkg/dag.go
@@ -1,13 +1,31 @@
 package pkg
 
-import "github.com/heimdalr/dag"
+import (
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/heimdalr/dag"
+)
 
 type Dag struct {
 	*dag.DAG
 }
 
-func newDag() *Dag {
-	return &Dag{
+func newDag(blocks []block) (*Dag, error) {
+	g := &Dag{
 		DAG: dag.NewDAG(),
 	}
+	var walkErr error
+	for _, b := range blocks {
+		err := g.AddVertexByID(blockAddress(b.HclSyntaxBlock()), b)
+		if err != nil {
+			walkErr = multierror.Append(walkErr, err)
+		}
+	}
+	for _, b := range blocks {
+		diag := hclsyntax.Walk(b.HclSyntaxBlock().Body, dagWalker{dag: g, rootBlock: b})
+		if diag.HasErrors() {
+			walkErr = multierror.Append(walkErr, diag.Errs()...)
+		}
+	}
+	return g, walkErr
 }
