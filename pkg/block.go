ba81f45efe74aa8b5ffa8f176e8c1fc88d587e1f
hezijie <lonegunmanb@hotmail.com>
2023-12-12 21:35:01 +0800

    commit for now

--- pkg/block.go
+++ pkg/block.go
@@ -41,11 +41,8 @@ func decode(b block) error {
 	defaults.SetDefaults(b)
 	hb := b.HclBlock()
 	evalContext := b.EvalContext()
-	isString := false
 	if hb.forEach != nil {
 		evalContext = evalContext.NewChild()
-		isString = hb.forEach.value.Type() == cty.String
-		println(isString)
 		evalContext.Variables = map[string]cty.Value{
 			"each": cty.ObjectVal(map[string]cty.Value{
 				"key":   cty.StringVal(CtyValueToString(hb.key)),
