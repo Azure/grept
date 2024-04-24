bd5e9652ba30f96d9151c23a01c94de3aaa5fd69
hezijie <lonegunmanb@hotmail.com>
2024-04-24 16:32:51 +0800

    add github repository teams data source

---
+++ pkg/data_github_repository_teams_test.go
@@ -0,0 +1,82 @@
+package pkg_test
+
+import (
+	"github.com/Azure/grept/pkg"
+	"github.com/Azure/grept/pkg/githubclient"
+	"github.com/google/go-github/v61/github"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"os"
+	"testing"
+
+	"github.com/prashantv/gostub"
+)
+
+func TestGithubRepositoryTeamsRead(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockRepositoriesClient := NewMockRepositoriesClient(ctrl)
+	owner := "grept"
+	name := "test"
+	sut := &pkg.GitHubRepositoryTeamsDatasource{
+		Owner:    owner,
+		RepoName: name,
+	}
+	stub := gostub.Stub(&githubclient.GetGithubClient, func() (*githubclient.Client, error) {
+		mockRepositoriesClient.EXPECT().ListTeams(gomock.Any(), gomock.Eq(owner), gomock.Eq(name), gomock.Any()).Return([]*github.Team{
+			{
+				Name:       p(name),
+				Slug:       p("slug"),
+				Permission: p("permission"),
+			},
+		}, &github.Response{}, nil).Times(1)
+		return &githubclient.Client{
+			Repositories: func() githubclient.RepositoriesClient {
+				return mockRepositoriesClient
+			},
+		}, nil
+	})
+	defer stub.Reset()
+	err := sut.ExecuteDuringPlan()
+	require.NoError(t, err)
+	assert.Len(t, sut.Teams, 1)
+	assert.Equal(t, name, sut.Teams[0].Name)
+	assert.Equal(t, "slug", sut.Teams[0].Slug)
+	assert.Equal(t, "permission", sut.Teams[0].Permission)
+}
+
+func TestGithubRepositoryTeamsRead_IntegrationTest(t *testing.T) {
+	if os.Getenv("GITHUB_TOKEN") == "" {
+		t.Skip("to run this test you must set env GITHUB_TOKEN first")
+	}
+	owner := readEssentialEnv(t, "GITHUB_REPOSITORY_TEAMS_DATASOURCE_INTEGRATION_TEST_OWNER")
+	repoName := readEssentialEnv(t, "GITHUB_REPOSITORY_TEAMS_DATASOURCE_INTEGRATION_TEST_REPO_NAME")
+	expectedTeam := readEssentialEnv(t, "GITHUB_REPOSITORY_TEAMS_DATASOURCE_INTEGRATION_TEST_EXPECTED_TEAM")
+
+	sut := &pkg.GitHubRepositoryTeamsDatasource{
+		Owner:    owner,
+		RepoName: repoName,
+	}
+	err := sut.ExecuteDuringPlan()
+	require.NoError(t, err)
+	matched := false
+	for _, team := range sut.Teams {
+		if team.Name == expectedTeam {
+			matched = true
+			break
+		}
+	}
+	assert.True(t, matched)
+}
+
+func readEssentialEnv(t *testing.T, envName string) string {
+	r := os.Getenv(envName)
+	if r == "" {
+		t.Skipf("to run this test you must set env %s first", envName)
+	}
+	return r
+}
+
+func p(s string) *string {
+	return &s
+}
---
+++ pkg/githubclient/client.go
@@ -0,0 +1,56 @@
+//go:generate mockgen -destination ../mock_repositories_client_test.go -package pkg_test . RepositoriesClient
+package githubclient
+
+import (
+	"context"
+	"fmt"
+	"github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v61/github"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Client struct {
+	*github.Client
+	Repositories func() RepositoriesClient
+}
+
+var GetGithubClient = func() (*Client, error) {
+	if githubToken := os.Getenv("GITHUB_TOKEN"); githubToken != "" {
+		return newClient(github.NewClient(nil).WithAuthToken(githubToken)), nil
+	}
+	ghAppIntegrationIdRaw := os.Getenv("GITHUB_APP_INTEGRATION_ID")
+	integrationId, err := strconv.ParseInt(ghAppIntegrationIdRaw, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("incorrect GITHUB_APP_INTEGRATION_ID: %s", ghAppIntegrationIdRaw)
+	}
+	installationIdRaw := os.Getenv("GITHUB_APP_INSTALLATION_ID")
+	installationId, err := strconv.ParseInt(installationIdRaw, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("incorrect GITHUB_APP_INSTALLATION_ID: %s", installationIdRaw)
+	}
+	privateKey := os.Getenv("GITHUB_APP_PRIVATEKEY")
+	if privateKey == "" {
+		return nil, fmt.Errorf("must set env GITHUB_APP_PRIVATEKEY")
+	}
+	itr, err := ghinstallation.New(http.DefaultTransport, integrationId, installationId, []byte(privateKey))
+	if err != nil {
+		return nil, err
+	}
+	return newClient(github.NewClient(&http.Client{Transport: itr})), nil
+}
+
+func newClient(c *github.Client) *Client {
+	r := &Client{
+		Client: c,
+	}
+	r.Repositories = func() RepositoriesClient {
+		return r.Client.Repositories
+	}
+	return r
+}
+
+type RepositoriesClient interface {
+	ListTeams(ctx context.Context, owner string, repo string, opts *github.ListOptions) ([]*github.Team, *github.Response, error)
+}
