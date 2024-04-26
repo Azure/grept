55a968b9b51a3a5f481f7e3f25fadbb1e6427d37
hezijie <lonegunmanb@hotmail.com>
2024-04-26 17:25:56 +0800

    redesign fix.github_team_repository and corresponding testing fixture

--- pkg/fix_github_team_repository.go
+++ pkg/fix_github_team_repository.go
@@ -9,14 +9,17 @@ import (
 
 var _ Fix = &GitHubTeamRepositoryFix{}
 
+type TeamRepositoryBinding struct {
+	TeamSlug   string `hcl:"team_slug"`
+	Permission string `hcl:"permission,optional" default:"pull" validate:"oneof=pull triage push maintain admin"`
+}
+
 type GitHubTeamRepositoryFix struct {
 	*golden.BaseBlock
 	*BaseFix
-	Owner      string `hcl:"owner"`
-	RepoName   string `hcl:"repo_name"`
-	TeamSlug   string `hcl:"team_slug,optional" validate:"conflict_with=TeamId,at_least_one_of=TeamSlug TeamId"`
-	TeamId     int    `hcl:"team_id,optional" validate:"conflict_with=TeamSlug,at_least_one_of=TeamSlug TeamId"`
-	Permission string `hcl:"permission,optional" default:"pull" validate:"oneof=pull triage push maintain admin"`
+	Owner    string                  `hcl:"owner"`
+	RepoName string                  `hcl:"repo_name"`
+	Teams    []TeamRepositoryBinding `hcl:"team"`
 }
 
 func (g *GitHubTeamRepositoryFix) Type() string {
@@ -28,34 +31,34 @@ func (g *GitHubTeamRepositoryFix) Apply() error {
 	if err != nil {
 		return fmt.Errorf("cannot create github client: %s", err.Error())
 	}
+	teamClient := client.Teams()
 	org, _, err := client.Organizations().Get(g.Context(), g.Owner)
 	if err != nil {
 		return fmt.Errorf("cannot read org info for %s, %s must be an organization", g.Owner, g.Owner)
 	}
-	var teamId = int64(g.TeamId)
-	if teamId != 0 {
-		if err = g.checkTeamId(client, *org.ID, teamId); err != nil {
-			return err
+	var teamIds []int64
+	for _, team := range g.Teams {
+		teamId, err := g.checkTeamSlug(client, team.TeamSlug)
+		if err != nil {
+			return fmt.Errorf("cannot read team id for team %s", team.TeamSlug)
+		}
+		teamIds = append(teamIds, teamId)
+	}
+	teams, err := listTeamsForRepository(client, g.Owner, g.RepoName, g.Context())
+	if err != nil {
+		return fmt.Errorf("cannot read existing teams for %s/%s: %+v", g.Owner, g.RepoName, err)
+	}
+	for _, team := range teams {
+		if _, err := teamClient.RemoveTeamRepoByID(g.Context(), *org.ID, *team.ID, g.Owner, g.RepoName); err != nil {
+			return fmt.Errorf("error when remove team %s from repo %s: %+v", *team.Name, g.RepoName, err)
 		}
 	}
-	teamId, err = g.checkTeamSlug(client, g.TeamSlug)
-	if err != nil {
-		return err
-	}
-	githubTeams, err := listTeamsForRepository(client, g.Owner, g.RepoName, g.Context())
-	if err != nil {
-		return err
-	}
-	for _, team := range githubTeams {
-		if *team.ID == teamId {
-			return nil
+	for i, newTeamId := range teamIds {
+		if _, err := teamClient.AddTeamRepoByID(g.Context(), *org.ID, newTeamId, g.Owner, g.RepoName, &github.TeamAddTeamRepoOptions{Permission: g.Teams[i].Permission}); err != nil {
+			return fmt.Errorf("error when add team %s to repo %s/%s: %+v", g.Teams[i].TeamSlug, g.Owner, g.RepoName, err)
 		}
 	}
-
-	_, err = client.Teams().AddTeamRepoByID(g.Context(), *org.ID, teamId, g.Owner, g.RepoName, &github.TeamAddTeamRepoOptions{
-		Permission: g.Permission,
-	})
-	return err
+	return nil
 }
 
 func (g *GitHubTeamRepositoryFix) checkTeamSlug(client *githubclient.Client, slug string) (int64, error) {
@@ -64,9 +67,4 @@ func (g *GitHubTeamRepositoryFix) checkTeamSlug(client *githubclient.Client, slu
 		return -1, err
 	}
 	return *team.ID, nil
-}
-
-func (g *GitHubTeamRepositoryFix) checkTeamId(client *githubclient.Client, orgId, teamId int64) error {
-	_, _, err := client.Teams().GetTeamByID(g.Context(), orgId, teamId)
-	return err
 }
