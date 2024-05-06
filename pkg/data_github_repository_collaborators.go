4537be93e0633505bbc285c8e7c2047a83405fd7
zjhe <lonegunmanb@hotmail.com>
2024-05-06 18:12:43 +0800

    add fix.github_repository_collaborators

--- pkg/data_github_repository_collaborators.go
+++ pkg/data_github_repository_collaborators.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/Azure/golden"
@@ -11,13 +12,14 @@ import (
 var _ Data = &GitHubRepositoryTeamsDatasource{}
 
 type User struct {
-	Name    string `attribute:"name"`
-	Company string `attribute:"company"`
-	Id      int64  `attribute:"id"`
-	Login   string `attribute:"login"`
-	Email   string `attribute:"email"`
-	Type    string `attribute:"type"`
-	URL     string `attribute:"url"`
+	Name        string   `attribute:"name"`
+	Company     string   `attribute:"company"`
+	Id          int64    `attribute:"id"`
+	Login       string   `attribute:"login"`
+	Email       string   `attribute:"email"`
+	Type        string   `attribute:"type"`
+	URL         string   `attribute:"url"`
+	Permissions []string `attribute:"permissions"`
 }
 
 type GitHubRepositoryCollaboratorsDatasource struct {
@@ -37,14 +39,27 @@ func (g *GitHubRepositoryCollaboratorsDatasource) ExecuteDuringPlan() error {
 	if err != nil {
 		return fmt.Errorf("cannot create github client: %s", err.Error())
 	}
-	opts := &github.ListCollaboratorsOptions{ListOptions: github.ListOptions{PerPage: 100}}
+	collaborators, err := listRepositoryCollaborators(client, g.Context(), g.Owner, g.RepoName)
+	if err != nil {
+		return err
+	}
+	g.Collaborators = collaborators
+	return nil
+}
+
+func listRepositoryCollaborators(client *githubclient.Client, ctx context.Context, owner, repoName string) ([]User, error) {
+	var r []User
+	opts := &github.ListCollaboratorsOptions{
+		Affiliation: "direct",
+		ListOptions: github.ListOptions{PerPage: 100},
+	}
 	for {
-		collaborators, resp, err := client.Repositories.ListCollaborators(g.Context(), g.Owner, g.RepoName, opts)
+		collaborators, resp, err := client.Repositories.ListCollaborators(ctx, owner, repoName, opts)
 		if err != nil {
-			return fmt.Errorf("cannot list collaborators for %s/%s: %s", g.Owner, g.RepoName, err.Error())
+			return nil, fmt.Errorf("cannot list collaborators for %s/%s: %s", owner, repoName, err.Error())
 		}
 		for _, c := range collaborators {
-			g.Collaborators = append(g.Collaborators, User{
+			user := User{
 				Name:    value(c.Name),
 				Company: value(c.Company),
 				Id:      value(c.ID),
@@ -52,14 +67,20 @@ func (g *GitHubRepositoryCollaboratorsDatasource) ExecuteDuringPlan() error {
 				Email:   value(c.Email),
 				Type:    value(c.Type),
 				URL:     value(c.URL),
-			})
+			}
+			for p, enabled := range c.Permissions {
+				if enabled {
+					user.Permissions = append(user.Permissions, p)
+				}
+			}
+			r = append(r, user)
 		}
 		if resp.NextPage == 0 {
 			break
 		}
 		opts.Page = resp.NextPage
 	}
-	return nil
+	return r, nil
 }
 
 func value[T any](p *T) T {
---
+++ pkg/fix_github_repository_collaborators_test.go
@@ -0,0 +1,99 @@
+package pkg_test
+
+import (
+	"context"
+	"github.com/Azure/grept/pkg"
+	"github.com/Azure/grept/pkg/githubclient"
+	"github.com/google/go-github/v61/github"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestGithubRepositoryCollaboratorsFix_IntegrationTest(t *testing.T) {
+	testToken := os.Getenv("INTEGRATION_TEST_GITHUB_TOKEN")
+	if testToken == "" {
+		t.Skip("to run this test you must set env INTEGRATION_TEST_GITHUB_TOKEN first")
+	}
+	t.Setenv("GITHUB_TOKEN", testToken)
+	client, err := githubclient.GetGithubClient()
+	require.NoError(t, err)
+	owner := readEssentialEnv(t, "INTEGRATION_TEST_GITHUB_OWNER")
+	repoName := readEssentialEnv(t, "INTEGRATION_TEST_GITHUB_REPO_NAME")
+	expectedUserName := readEssentialEnv(t, "INTEGRATION_TEST_GITHUB_EXPECTED_USER_NAME")
+	altExpectedUserName := readEssentialEnv(t, "INTEGRATION_TEST_GITHUB_ALT_EXPECTED_USER_NAME")
+
+	defer func() {
+		collaborators, _, err := client.Repositories.ListCollaborators(context.Background(), owner, repoName, &github.ListCollaboratorsOptions{
+			ListOptions: github.ListOptions{PerPage: 100},
+		})
+		require.NoError(t, err)
+		for _, collaborator := range collaborators {
+			invitation, _ := pkg.FindRepoInvitation(client, context.Background(), owner, repoName, *collaborator.Login)
+			if invitation != nil {
+				_, err = client.Repositories.DeleteInvitation(context.Background(), owner, repoName, invitation.GetID())
+				require.NoError(t, err)
+			}
+			_, err = client.Repositories.RemoveCollaborator(context.Background(), owner, repoName, *collaborator.Login)
+			require.NoError(t, err)
+		}
+		_, _, err = client.Repositories.AddCollaborator(context.Background(), owner, repoName, expectedUserName, &github.RepositoryAddCollaboratorOptions{
+			Permission: "admin",
+		})
+		require.NoError(t, err)
+	}()
+
+	cases := []map[string]string{
+		{},
+		{
+			expectedUserName: "admin",
+		},
+		{
+			expectedUserName:    "admin",
+			altExpectedUserName: "pull",
+		},
+		{
+			expectedUserName: "admin",
+		},
+		{
+			altExpectedUserName: "pull",
+		},
+		{},
+	}
+	for _, expectedCollaborators := range cases {
+		sut := &pkg.GitHubRepositoryCollaboratorsFix{
+			Owner:    owner,
+			RepoName: repoName,
+		}
+		for name, permission := range expectedCollaborators {
+			sut.Collaborators = append(sut.Collaborators, pkg.CollaboratorForRepositoryCollaboratorsFix{
+				Name:       name,
+				Permission: permission,
+			})
+		}
+		err = sut.Apply()
+		require.NoError(t, err)
+		assertCollaborators(t, client, owner, repoName, expectedCollaborators)
+	}
+}
+
+func assertCollaborators(t *testing.T, client *githubclient.Client, owner, repoName string, collaborators map[string]string) {
+	result, _, err := client.Repositories.ListCollaborators(context.Background(), owner, repoName, &github.ListCollaboratorsOptions{
+		Affiliation: "direct",
+		ListOptions: github.ListOptions{PerPage: 100},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, len(collaborators), len(result))
+	for _, user := range result {
+		expectedPermission, ok := collaborators[*user.Login]
+		require.True(t, ok)
+		var matched = false
+		for permission, enabled := range user.Permissions {
+			if enabled && expectedPermission == permission {
+				matched = true
+			}
+		}
+		assert.True(t, matched)
+	}
+}
